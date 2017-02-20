5903d9afbb9f73f6481d513cac405a3dc5cf089b
Boshen Cui <boshen.cui@gmail.com>
2017-02-19 20:06:33 -0500

    ghost models, updated 2d map

--- js/pacman.js
+++ js/pacman.js
@@ -1,19 +1,3 @@
-var key_map = {};
-var camera, scene, renderer;
-var geometry, material, mesh;
-var controls;
-var accel = 400.0;
-var wall = [];
-var raycaster;
-var element = document.body;
-
-var prevTime = performance.now();
-var time = performance.now();
-var delta;
-var pos;
-
-var velocity = new THREE.Vector3();
-
 on_key_down = on_key_up = function(e) {
     e = e || event; // to deal with IE
     key_map[e.keyCode] = e.type == 'keydown';
@@ -34,57 +18,108 @@ test_keys = function() {
     return true;
 }
 
-var map_2d = {};
-map_2d.px = 5;
 
-setup_2d = function() {
+setup_map = function() {
     var canvas = document.getElementById("map-2d");
     var ctx = canvas.getContext("2d");
 
+    map_2d.canvas = canvas;
+    map_2d.ctx = ctx;
+
     canvas.height = map_2d.px * map[0].length;
     canvas.width = map_2d.px * map.length;
+
+
+    // floor
+    floor_geometry = new THREE.PlaneGeometry(map.length * 10, map[0].length * 10);
+    floor_geometry.rotateX(-Math.PI / 2);
+
+    floor_material = new THREE.MeshBasicMaterial({ color: "#ffffff" });
+    floor = new THREE.Mesh(floor_geometry, floor_material);
+    floor.position.y = 5;
+    floor.position.z = -5;
+    floor.position.x = -5;
+    scene.add(floor);
+
+
+
+    wall_geometry = new THREE.BoxGeometry(10, 10, 10);
+    wall_material = new THREE.MeshBasicMaterial({ color: "#1a237e", side: THREE.DoubleSide });
+
+    dot_geometry = new THREE.SphereGeometry(1, 10, 10);
+    dot_material = new THREE.MeshBasicMaterial({ color: "#fff000", side: THREE.DoubleSide });
+
+    powerPellet_geometry = new THREE.SphereGeometry(3, 10, 10);
+    powerPellet_material = new THREE.MeshBasicMaterial({ color: "#ffa500", side: THREE.DoubleSide });
+
+
+
     for (var i = 0; i < map.length; i++) {
         for (var j = 0; j < map[i].length; j++) {
-            if (map[i][j] == '0') {
+
+            var type = map[i][j];
+            var x = (i - map.length / 2) * 10;
+            var y = 10;
+            var z = (j - map[i].length / 2) * 10;
+            if (type == '0') {
+                wallUnit = new THREE.Mesh(wall_geometry, wall_material);
+                wall.push(wallUnit);
+
+                scene.add(wallUnit);
+                wallUnit.position.x = x;
+                wallUnit.position.y = y;
+                wallUnit.position.z = z;
+
                 ctx.fillStyle = "#FFFFFF";
-            } else
+                ctx.fillRect(i * map_2d.px, j * map_2d.px, map_2d.px, map_2d.px);
+            } else if (type == '*') {
+
+                dotUnit = new THREE.Mesh(dot_geometry, dot_material);
+                dotUnit.dotType = "normal";
+
+                dots.push(dotUnit);
+                scene.add(dotUnit);
+                dotUnit.position.x = x;
+                dotUnit.position.y = y;
+                dotUnit.position.z = z;
+
+
                 ctx.fillStyle = "#000000";
+                ctx.fillRect(i * map_2d.px, j * map_2d.px, map_2d.px, map_2d.px);
 
-            ctx.fillRect(i * map_2d.px, j * map_2d.px, map_2d.px, map_2d.px);
+                draw_circle((i + 0.5) * map_2d.px, (j + 0.5) * map_2d.px, (map_2d.px-4)/2, "#fff000");
+            } else if (type == 'X') {
 
-            console.log(i * map_2d.px, j * map_2d.px)
+                dotUnit = new THREE.Mesh(powerPellet_geometry, powerPellet_material);
+                dotUnit.dotType = "powerPellet";
+
+                dots.push(dotUnit);
+                scene.add(dotUnit);
+
+                dotUnit.position.x = x;
+                dotUnit.position.y = y;
+                dotUnit.position.z = z;
+
+
+
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(i * map_2d.px, j * map_2d.px, map_2d.px, map_2d.px);
+
+                draw_circle((i + 0.5) * map_2d.px, (j + 0.5)* map_2d.px, (map_2d.px-2)/2, "#ffa500");
+            } else {
+                ctx.fillStyle = "#000000";
+                ctx.fillRect(i * map_2d.px, j * map_2d.px, map_2d.px, map_2d.px);
+            }
+
+
+
         }
     }
 
-    map_2d.canvas = canvas;
-    map_2d.ctx = ctx;
+    totalDots = dots.length;
 }
 
-prevCoords = [0, 0];
 
-draw_2d = function(pos, color) {
-    try {
-        if (map[prevCoords[0]][prevCoords[1]] == "0")
-            map_2d.ctx.fillStyle = "#ffffff";
-        else
-            map_2d.ctx.fillStyle = "#000000";
-        map_2d.ctx.fillRect(prevCoords[0] * map_2d.px, prevCoords[1] * map_2d.px, map_2d.px, map_2d.px);
-
-        var coords = get_cell(pos);
-        map_2d.ctx.fillStyle = color;
-        map_2d.ctx.fillRect(coords[0] * map_2d.px, coords[1] * map_2d.px, map_2d.px, map_2d.px);
-        prevCoords = coords;
-    } catch (err) {}
-
-}
-
-get_cell = function(pos) {
-
-    var x = Math.round(pos.x / 10 + map.length / 2);
-    var y = Math.round(pos.z / 10 + map[0].length / 2);
-
-    return [x, y]
-}
 
 document.addEventListener('keydown', on_key_down, false);
 document.addEventListener('keyup', on_key_up, false);
@@ -110,7 +145,13 @@ THREE.PointerLockControls = function(camera) {
         new THREE.Vector3(1, 0, 0),
         new THREE.Vector3(0, 0, -1),
         new THREE.Vector3(-1, 0, 0),
+        new THREE.Vector3(1, 0, 1),
+        new THREE.Vector3(1, 0, -1),
+        new THREE.Vector3(-1, 0, -1),
+        new THREE.Vector3(-1, 0, 1),
     ];
+
+    this.prevCoords = [0, 0];
 
     var onMouseMove = function(event) {
 
@@ -204,7 +245,6 @@ animate();
 
 function init() {
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-    setup_2d();
     scene = new THREE.Scene();
 
     var light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 0.75);
@@ -214,37 +254,13 @@ function init() {
     scene.add(controls.getObject());
 
     pos = controls.getObject().position;
-    pos.x = 140;
+    pos.x = 75;
     pos.y = 10;
     pos.z = -5;
 
+    setup_map();
 
-    // floor
-    floor_geometry = new THREE.PlaneGeometry(2000, 2000);
-    floor_geometry.rotateX(-Math.PI / 2);
-
-    wall_geometry = new THREE.BoxGeometry(10, 10, 10);
-
-
-    floor_material = new THREE.MeshBasicMaterial({ color: "#1a237e" });
-    floor = new THREE.Mesh(floor_geometry, floor_material);
-    scene.add(floor);
-
-    for (var i = 0; i < map.length; i++) {
-        for (var j = 0; j < map[i].length; j++) {
-            if (map[i][j] == '0') {
-                wall_material = new THREE.MeshBasicMaterial({ color: "#ffffff", side: THREE.DoubleSide });
-                wallUnit = new THREE.Mesh(wall_geometry, wall_material);
-
-                wall.push(wallUnit);
-
-                scene.add(wallUnit);
-                wallUnit.position.x = (i - map.length / 2) * 10;
-                wallUnit.position.y = 10;
-                wallUnit.position.z = (j - map[i].length / 2) * 10;
-            }
-        }
-    }
+    setup_ghosts();
     //
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor("#000000");
@@ -263,24 +279,45 @@ function onWindowResize() {
 }
 
 
-var intersects = [],
+var wallIntersects = [],
+    dotIntersects = [],
+    ghostIntersects = [],
     arrow = [];
 var len = 4;
+var toggleFloor = false;
+var animationFrameId;
 
 
 var draw = false;
 
-function testRay(d) {
+function testRay(d, group) {
 
     var raycaster = new THREE.Raycaster(pos, d, 0, len);
 
     if (draw) {
         arrow[2] = new THREE.ArrowHelper(d, pos, len, "#FFF000");
+        arrow[2].name = "ArrowHelper";
         scene.add(arrow[2]);
     }
 
-    return raycaster.intersectObjects(wall);
+    return raycaster.intersectObjects(group);
 }
+
+function deleteAllArrows() {
+    var toBeDeleted = [];
+    scene.children.map(function(c) {
+        if (c.name == "ArrowHelper") {
+            toBeDeleted.push(c);
+        }
+    });
+
+    toBeDeleted.map(function(c) {
+        scene.remove(c);
+        delete c;
+    })
+    console.log("geometries=" + this.renderer.info.memory.geometries + " programs=" + this.renderer.info.memory.programs + " textures=" + this.renderer.info.memory.textures);
+}
+
 
 function getComponents(d) {
     var v = new THREE.Vector3();
@@ -293,24 +330,31 @@ function getComponents(d) {
     x = new THREE.Vector3(dd.x, 0, 0);
     z = new THREE.Vector3(0, 0, dd.z);
 
-    if(draw){
+    if (draw) {
         arrow[0] = new THREE.ArrowHelper(dd, pos, len, "#FAC123");
+        arrow[0].name = "ArrowHelper";
         scene.add(arrow[0])
 
         arrow[1] = new THREE.ArrowHelper(x, pos, Math.max(dd.x, 3), "#FF0000");
+        arrow[1].name = "ArrowHelper";
         scene.add(arrow[1]);
-    
+
         arrow[2] = new THREE.ArrowHelper(z, pos, Math.max(dd.z, 3), "#0000FF");
+        arrow[2].name = "ArrowHelper";
         scene.add(arrow[2]);
     }
     return dd;
 }
 
 function animate() {
-    requestAnimationFrame(animate);
-    if (controls.enabled) {
-        draw_2d(pos, "#FAC123");
+    animiationFrameId = requestAnimationFrame(animate);
+    stats.begin();
+    stats2.begin();
 
+    if (controls.enabled) {
+        draw_2d(pos, controls.prevCoords, "#FFEE00", false);
+
+        dotIntersects = [];
 
         time = performance.now();
         delta = (time - prevTime) / 1000;
@@ -318,6 +362,10 @@ function animate() {
         velocity.x -= velocity.x * 10.0 * delta;
         velocity.z -= velocity.z * 10.0 * delta;
         velocity.y -= velocity.y * 10.0 * delta;
+
+
+        draw = test_keys("1");
+        toggleFloor = test_keys("2");
 
         if (test_keys("W")) {
             velocity.z -= accel * delta;
@@ -331,33 +379,73 @@ function animate() {
         if (test_keys("D")) {
             velocity.x += accel * delta;
         }
-        if( test_keys("1")){
-            draw = !draw;
+        if (test_keys("3")) {
+            pos.y = 10;
         }
-
-
         if (test_keys("SHIFT")) velocity.y += accel * delta;
-        if (test_keys("SPACE") && pos.y > 10) velocity.y -= accel * delta;
-        if (test_keys("SPACE") && pos.y <= 10) velocity.y = 0;
+
+        if (test_keys("SPACE")) velocity.y -= accel * delta;
+
+        if (test_keys("SPACE") && pos.y <= 10 && !toggleFloor) {
+            velocity.y = 0;
+        }
 
 
         vectors = getComponents(velocity);
 
-        for (var i = 0; i < 4; i++) {
-            intersects[i] = testRay(controls.rays[i]);
+        //check collisions with walls, dots, and ghosts
+        for (var i = 0; i < 8; i++) {
+            wallIntersects[i] = testRay(controls.rays[i], wall);
+            dotIntersects = dotIntersects.concat(testRay(controls.rays[i], dots));
+            ghostIntersects = ghostIntersects.concat(testRay(controls.rays[i], ghosts));
         }
 
-        if ((intersects[1].length == 0 && vectors.x > 0) || (intersects[3].length == 0 && vectors.x < 0) ) {
+        //move player
+        if ((wallIntersects[1].length == 0 && vectors.x > 0) || (wallIntersects[3].length == 0 && vectors.x < 0)) {
             pos.x += vectors.x * delta;
-        } 
-        if ((intersects[0].length == 0 && vectors.z > 1) || (intersects[2].length == 0 && vectors.z < -1)) {
+        }
+        if ((wallIntersects[0].length == 0 && vectors.z > 1) || (wallIntersects[2].length == 0 && vectors.z < -1)) {
             pos.z += vectors.z * delta;
+        }
+
+        if (pos.z < -145) {
+            pos.z = 130;
+        }
+
+        if (pos.z > 135) {
+            pos.z = -140;
         }
 
         controls.getObject().translateY(velocity.y * delta);
 
+        //eat dots
+        if (dotIntersects.length > 0) {
+
+            dots.lastDotEaten = dotIntersects[0].object.dotType;
+
+            //delete dot from the scene
+            scene.remove(dotIntersects[0].object);
+            dots = dots.filter(function(e) {
+                return e !== dotIntersects[0].object });
+            //delete dot from the map
+            var coords = get_cell(dotIntersects[0].object.position);
+            map[coords[0]][coords[1]] = " ";
+
+            delete dotIntersects[0].object;
+        }
+
+        //if player has collided with a ghost, game is over
+        if(ghostIntersects.length > 0){
+            document.exitPointerLock();
+            cancelAnimationFrame(animiationFrameId);
+        }
+
+        ghost_tick(pos, dots, delta);
         prevTime = time;
     }
 
     renderer.render(scene, camera);
+
+    stats.end();
+    stats2.end();
 }
